9a1ec671db8f32d2023415ebcd3fa2ee30c83762
Thiago Lagden <lagden@gmail.com>
2024-11-22 12:35:37 -0300

    flatou o build

--- dist/App.svelte
+++ dist/App.svelte
@@ -1,6 +1,14 @@
 <script>
+    import {tick} from 'svelte'
     import {Loading, acts as loadingActs} from '@tadashi/svelte-loading'
-    loadingActs.show(processing)
+
+    tick().then(() => {
+        loadingActs.show(true)
+        setTimeout(() => {
+            loadingActs.show(false)
+        }, 1000)
+    })
 </script>
 
 <Loading animation="ChaoticOrbit" />
+<h1>Estamos no app</h1>
